8bb1e8c0275c9b282e8110646fd19163b595b041
Catriel Nanthaveth <cnanthaveth@shalion.com>
2025-06-10 22:04:32 -0300

    first commit

---
+++ src/app/fechas/page.jsx
@@ -0,0 +1,3 @@
+export default function FechasPage() {
+  return <div className="p-8 text-2xl">Página de las Fechas</div>;
+} 
---
+++ src/app/torneo/page.jsx
@@ -0,0 +1,3 @@
+export default function TorneoPage() {
+  return <div className="p-8 text-2xl">Página del Torneo</div>;
+}
